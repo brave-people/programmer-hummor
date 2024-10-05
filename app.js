f1063e323ce9e7fa9b8e4c2cf6be0a59ad7803aa
고언약[KR Server Tech] <covenant.ko@kakao.com>
2024-10-05 16:58:27 +0900

    header, footer 추가

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 async function fetchHumorFeed() {
     try {
+        const response = await fetch('humor_data.json');
+        
         // const apiUrl = process.env.REAL_API_HOST || 'humor_data.json';
-        const apiUrl = 'https://real-brave-people.p-e.kr';
-        const response = await fetch(apiUrl + '/front/v1/humors?orderType=RECENTLY&langType=ENG,KO');
+        // const apiUrl = 'https://real-brave-people.p-e.kr';
+        // const response = await fetch(apiUrl + '/front/v1/humors?orderType=RECENTLY&langType=ENG,KO');
         if (!response.ok) {
             throw new Error('fail to call API');
         }
